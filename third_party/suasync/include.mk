cb8bb2db2cd8837493042dff75282f5865cb09b3
ilivoo <316488140@qq.com>
2020-08-19 20:05:45 +0800

    change maven rep

--- third_party/suasync/include.mk
+++ third_party/suasync/include.mk
@@ -15,7 +15,7 @@
 
 SUASYNC_VERSION := 1.4.0
 SUASYNC := third_party/suasync/async-$(SUASYNC_VERSION).jar
-SUASYNC_BASE_URL := https://repo1.maven.org/maven2/com/stumbleupon/async/$(SUASYNC_VERSION)
+SUASYNC_BASE_URL := http://maven.aliyun.com/nexus/content/groups/public/com/stumbleupon/async/$(SUASYNC_VERSION)
 
 $(SUASYNC): $(SUASYNC).md5
 	set dummy "$(SUASYNC_BASE_URL)" "$(SUASYNC)"; shift; $(FETCH_DEPENDENCY)
